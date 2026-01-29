a0c2a4d7c3d4acde42dfa1c8765e771c01c0e744
othiagopro <thiagomendo.ti@gmail.com>
2026-01-29 19:13:56 -0400

    Alterações na pagina inicial

--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    
+    dados = {
+        1: {'nome': "Nebulosa de Carina", 
+            'legenda': "webbtelecope.org / NASA / James Webb"},
+        2: {'nome': "Nebulosa de Orion", 
+            'legenda': "webbtelecope.org / NASA / James Webb"},
+    }
+
+    return render(request, 'galeria/index.html', {'cards': dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
