9aab83d6eae96969e3f327cc5e94557abc5e4ed0
othiagopro <thiagomendo.ti@gmail.com>
2026-02-09 19:50:09 -0400

    Finalizado tela de cadastro e login

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -5,10 +5,68 @@ class LoginForms(forms.Form):
         label='Nome de Login',
         required=True,
         max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite seu nome de login',
+            }
+        ),
     )
     senha=forms.CharField(
         label='Senha',
         required=True,
         max_length=70,
-        widget=forms.PasswordInput(),
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha', 
+            }
+        ),
     )
+
+class CadastroForms(forms.Form):
+    nome_cadastro=forms.CharField(
+        label='Nome de Cadastro',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite seu nome completo',
+            }
+        ),
+    )
+    email=forms.EmailField(
+        label='Digite um email válido',
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite seu email',
+            }
+        ),
+    )
+    senha_cadastro=forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha', 
+            }
+        ),
+    )
+    senha_cadastro2=forms.CharField(
+        label='Confirme sua senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha novamente', 
+            }
+        ),
+    )
+
