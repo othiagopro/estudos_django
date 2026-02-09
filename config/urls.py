90b90bb63937f931c557c9f58b279682bca39c54
othiagopro <thiagomendo.ti@gmail.com>
2026-02-08 21:57:23 -0400

    Iniciando a parte de login

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('galeria.urls'))
+    path('', include('galeria.urls')), # Inclui as URLs do aplicativo 'galeria' para lidar com rotas relacionadas à galeria de fotografias
+    path('', include('usuarios.urls')), # Inclui as URLs do aplicativo 'usuarios' para lidar com rotas relacionadas a usuários
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
--- galeria/views.py
+++ galeria/views.py
@@ -8,7 +8,7 @@ def index(request):
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id) # Primeiro buscamos a fotografia pelo ID
-    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia}) 
 
 def buscar(request):
     fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
